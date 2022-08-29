081a6358d0191a0e583ea6f206d5b9ce2b1cce8a
naveenmalakala <naveenmalakala>
2022-08-29 10:08:42 +0000

    jobbyapp

---
+++ src/components/Jobs/index.js
@@ -0,0 +1,14 @@
+import Header from '../Header'
+
+import JobProfileSection from '../JobProfileSection'
+
+const Jobs = () => (
+  <>
+    <Header />
+    <div className="job-profile-container">
+      <JobProfileSection />
+    </div>
+  </>
+)
+
+export default Jobs
